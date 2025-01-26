71b5c4e93e69e018a52806b483f574f5f5bb024d
Korneliia Mushak <jackiemaistrenko@gmail.com>
2025-01-26 22:08:48 +0100

    create a project

--- src/views/Admin/dashboard/Dashboard.jsx
+++ src/views/Admin/dashboard/Dashboard.jsx
@@ -2,9 +2,11 @@ import s from "./Dashboard.module.scss";
 import InfoTile from "../../../components/ui/statisitc/infoTile/InfoTile.jsx";
 import PanelTitle from "../../../components/layot/panelTitle/PanelTitle.jsx";
 import LikeUnlikeCharBlock from "../../../components/features/statistics/likeUnlikeCharBlock/LikeUnlikeCharBlock.jsx";
-import GeneralStatsCharBlock from "../../../components/features/statistics/generalStatsCharBlock/GeneralStatsCharBlock.jsx";
+import GeneralStatsCharBlock
+    from "../../../components/features/statistics/generalStatsCharBlock/GeneralStatsCharBlock.jsx";
 import OutletPanelScroll from "../../../components/ui/outletPanelScroll/OutletPanelScroll.jsx";
-import MarketplacesOrdersCharBlock from "../../../components/features/statistics/marketplacesOrdersCharBlock/MarketplacesOrdersCharBlock.jsx";
+import MarketplacesOrdersCharBlock
+    from "../../../components/features/statistics/marketplacesOrdersCharBlock/MarketplacesOrdersCharBlock.jsx";
 import useFetch from "../../../functions/useFetch.js";
 import {blockStatsInitialData} from "./blockStatsInitialData.js";
 import {CgMail, CgRead} from "react-icons/cg";
@@ -19,18 +21,21 @@ const Dashboard = (props) => {
 
     return (
         <>
-
-            <PanelTitle title={'Analytics Dashboard - This month'} subTitle={'See the latest analytics from your marketplaces'}/>
+            <PanelTitle title={'Аналітика - цього місяця'}
+                        subTitle={'Актуальні дані з Ваших маркетплейсів'}/>
             <OutletPanelScroll>
                 <div className={s.dashboardContainer}>
                     <div className={s.topRow}>
 
-                        <InfoTile icon={<CgMail/>} value={data.sendInMonth.current} secondValue={data.sendInMonth.change} title={'Send messages'}/>
-                        <InfoTile icon={<CgRead/>} value={data.readInMonth.current} secondValue={data.readInMonth.change} title={'Read messages'}
+                        <InfoTile icon={<CgMail/>} value={data.sendInMonth.current}
+                                  secondValue={data.sendInMonth.change} title={'Надіслані повідомлення'}/>
+                        <InfoTile icon={<CgRead/>} value={data.readInMonth.current}
+                                  secondValue={data.readInMonth.change} title={'Прочитані повідомлення'}
                                   secondValueColor={'red'}/>
-                        <InfoTile icon={<GiClick/>} value={data.clickInMonth.current} secondValue={data.clickInMonth.change} title={'Click messages'}
+                        <InfoTile icon={<GiClick/>} value={data.clickInMonth.current}
+                                  secondValue={data.clickInMonth.change} title={'Посилання відвідано'}
                                   secondValueColor={'red'}/>
-                        <InfoTile title={'Write comment'}/>
+                        <InfoTile title={'Написано відгуків'}/>
                     </div>
                     <div className={s.div3}>
                         <GeneralStatsCharBlock/>
