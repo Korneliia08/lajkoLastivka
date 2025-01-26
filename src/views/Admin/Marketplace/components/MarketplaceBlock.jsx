ab8b03fae7fbb1d459becc010471c7bf1d8c4cbb
AlfaGruisPL <alfagruis@gmail.com>
2025-01-26 22:15:45 +0100

    stage2

--- src/views/Admin/Marketplace/components/MarketplaceBlock.jsx
+++ src/views/Admin/Marketplace/components/MarketplaceBlock.jsx
@@ -40,8 +40,6 @@ function MarketplaceBlock({data, fetchData}) {
                     }
                 ]
             });
-
-
         } catch (err) {
             console.log(err);
         }
--- src/views/Admin/informationOfMarket/mainInformations/MainInformations.jsx
+++ src/views/Admin/informationOfMarket/mainInformations/MainInformations.jsx
@@ -4,9 +4,42 @@ import Statistics from "./statistics/Statistics.jsx";
 import {FaChartPie, FaLink} from "react-icons/fa";
 import {MdDeleteOutline} from "react-icons/md";
 import cn from "../../../../functions/cn.js";
+import {confirmAlert} from "react-confirm-alert";
+import api from "../../../../providers/interceptors/refreshToken.interceptor.js";
+import {NavLink, useNavigate, useParams} from "react-router-dom";
 
 
 const MainInformations = ({...props}) => {
+    const {id} = useParams()
+    const navigate = useNavigate()
+
+    async function deleteMarketplace() {
+        try {
+            confirmAlert({
+                title: 'Видалення магазину',
+                //todo tu dorobić nazwę
+                // message: 'Ви напевно хочете видалити магазин: ' + data.name,
+
+                buttons: [
+                    {
+                        label: 'Так',
+                        onClick: async () => {
+                            await api.delete(`stores/${id}`);
+                            navigate('./admin/marketplaces')
+                        }
+                    },
+                    {
+                        label: 'Ні',
+                        onClick: () => {
+                        }
+                    }
+                ]
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
     return (
         <div className={s.mainInformationsContainer}>
             <div className={s.leftBlock}>
@@ -14,18 +47,18 @@ const MainInformations = ({...props}) => {
                 <Statistics/>
             </div>
             <aside>
-                <div className={cn(s.block, s.blockForAfter)}>
+                <NavLink to={'./admin/statistic/' + id} className={cn(s.block, s.blockForAfter)}>
                     <FaChartPie className={s.icStyle}/>
                     <span>Статистики</span>
-                </div>
-                <div className={cn(s.block, s.blockForAfter)}>
+                </NavLink>
+                <a className={cn(s.block, s.blockForAfter)}>
                     <FaLink className={s.icStyle}/>
                     <span className={s.link}>https://rozetka.com.ua</span>
-                </div>
-                <div className={s.block}>
+                </a>
+                <button onClick={deleteMarketplace} className={s.block}>
                     <MdDeleteOutline className={s.icDelete}/>
                     <span className={s.deleteSpan}>Видалити магазин</span>
-                </div>
+                </button>
             </aside>
         </div>
     )
