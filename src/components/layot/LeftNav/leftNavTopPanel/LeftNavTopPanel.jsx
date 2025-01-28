bdb64f89af87ead63dbcd5447875f88d0ea66155
AlfaGruisPL <alfagruis@gmail.com>
2025-01-28 15:49:17 +0100

    stage2

--- src/components/layot/LeftNav/leftNavTopPanel/LeftNavTopPanel.jsx
+++ src/components/layot/LeftNav/leftNavTopPanel/LeftNavTopPanel.jsx
@@ -1,12 +1,14 @@
 import s from "./LeftNavTopPanel.module.scss";
 import { RiArrowGoBackFill } from "react-icons/ri";
 import InputWithIcon from "@/components/ui/form/inputWithIcon/InputWithIcon.jsx";
+import { useNavigate } from "react-router-dom";
 
 const LeftNavTopPanel = ({ ...props }) => {
+  const navigate = useNavigate();
   return (
     <div className={s.leftNavTopPanelContainer}>
       <h1>ЛайкоЛастівка</h1>
-      <button className={s.logOut}>
+      <button className={s.icon} onClick={() => navigate(-1)}>
         <RiArrowGoBackFill />
       </button>
       <InputWithIcon disabled={true} />
--- src/components/layot/panelTitle/PanelTitle.jsx
+++ src/components/layot/panelTitle/PanelTitle.jsx
@@ -1,15 +1,26 @@
 import s from "./PanelTitle.module.scss";
 
-
-const PanelTitle = ({title, subTitle, buttonText, buttonIcon, onClick, ...props}) => {
-    return (
-        <div className={s.panelTitleContainer}>
-            <div className={s.blockForTitle}>
-                <h3 className={s.title}>{title}</h3>
-                <h4 className={s.subTitle}>{subTitle}</h4>
-            </div>
-            <button className={s.btnStyle} onClick={onClick}>{buttonIcon}{buttonText}</button>
-        </div>
-    )
-}
+const PanelTitle = ({
+  title,
+  subTitle,
+  buttonText,
+  buttonIcon,
+  onClick,
+  ...props
+}) => {
+  return (
+    <div className={s.panelTitleContainer}>
+      <div className={s.blockForTitle}>
+        <h3 className={s.title}>{title}</h3>
+        <h4 className={s.subTitle}>{subTitle}</h4>
+      </div>
+      {buttonText && (
+        <button className={s.btnStyle} onClick={onClick}>
+          {buttonIcon}
+          {buttonText}
+        </button>
+      )}
+    </div>
+  );
+};
 export default PanelTitle;
