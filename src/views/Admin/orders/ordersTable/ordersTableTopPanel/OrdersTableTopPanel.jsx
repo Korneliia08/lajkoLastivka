b47d887aeca55c6e9313e808668ca66c39d38ccf
AlfaGruisPL <alfagruis@gmail.com>
2025-01-28 12:48:49 +0100

    stage2

--- src/views/Admin/orders/ordersTable/ordersTableTopPanel/OrdersTableTopPanel.jsx
+++ src/views/Admin/orders/ordersTable/ordersTableTopPanel/OrdersTableTopPanel.jsx
@@ -1,35 +1,42 @@
 import s from "./OrdersTableTopPanel.module.scss";
 import OrdersTableTopPanelSearchInput from "./ordersTableTopPanelSearchInput/OrdersTableTopPanelSearchInput.jsx";
 import OrdersTableTopPanelDatesRange from "./ordersTableTopPanelDatesRange/OrdersTableTopPanelDatesRange.jsx";
-import {useModalManager} from "../../../../../hooks/modalManager.js";
+import { useModalManager } from "../../../../../hooks/modalManager.js";
 import MyModal from "../../../../../components/features/myModal/MyModal.jsx";
 import ManualMessageDispatch from "./manualMessageDispatch/ManualMessageDispatch.jsx";
-import {useSelector} from "react-redux";
+import { useSelector } from "react-redux";
 
+const OrdersTableTopPanel = ({ ...props }) => {
+  const selectedOrders = useSelector((state) => state.orders.selectedOrders);
+  const { controller, toggleModal } = useModalManager();
 
-const OrdersTableTopPanel = ({...props}) => {
-    const selectedOrders = useSelector(state => state.orders.selectedOrders);
-    const {controller, toggleModal} = useModalManager()
-
-    return (
-        <>
-            <div className={s.ordersTableTopPanelContainer}>
-                <div className={s.title}>
-                    <h3>Orders:</h3>
-                    <p>All orders in marketplace</p>
-                </div>
-                <div className={s.controls}>
-
-                    <OrdersTableTopPanelSearchInput/>
-                    <OrdersTableTopPanelDatesRange/>
-                    <button disabled={selectedOrders.length === 0} onClick={() => toggleModal('ManualMessageDispatch')}>Send fast message</button>
-                </div>
-            </div>
-            <MyModal options={{defaultClose: true, headerTitle: 'Message dispatch',}} name={'ManualMessageDispatch'}
-                     controller={controller}>
-                <ManualMessageDispatch controller={controller}/><
-            /MyModal>
-        </>
-    )
-}
+  return (
+    <>
+      <div className={s.ordersTableTopPanelContainer}>
+        <div className={s.title}>
+          <h3>Orders:</h3>
+          <p>All orders in marketplace</p>
+        </div>
+        <div className={s.controls}>
+          <OrdersTableTopPanelSearchInput />
+          <OrdersTableTopPanelDatesRange />
+          <button
+            className={s.sendFast}
+            disabled={selectedOrders.length === 0}
+            onClick={() => toggleModal("ManualMessageDispatch")}
+          >
+            Send fast message
+          </button>
+        </div>
+      </div>
+      <MyModal
+        options={{ defaultClose: true, headerTitle: "Message dispatch" }}
+        name={"ManualMessageDispatch"}
+        controller={controller}
+      >
+        <ManualMessageDispatch controller={controller} />
+      </MyModal>
+    </>
+  );
+};
 export default OrdersTableTopPanel;
--- src/views/Admin/orders/ordersTable/ordersTableTopPanel/ordersTableTopPanelSearchInput/OrdersTableTopPanelSearchInput.jsx
+++ src/views/Admin/orders/ordersTable/ordersTableTopPanel/ordersTableTopPanelSearchInput/OrdersTableTopPanelSearchInput.jsx
@@ -1,37 +1,41 @@
-import {useDispatch, useSelector} from "react-redux";
-import {useRef, useState} from "react";
-import {setSearchInput} from "../../../ordersSlice.js";
+import { useDispatch, useSelector } from "react-redux";
+import { useRef, useState } from "react";
+import { setSearchInput } from "../../../ordersSlice.js";
+import s from "./OrdersTableTopPanelSearchInput.module.scss";
+import InputWithIcon from "@/components/ui/form/inputWithIcon/InputWithIcon.jsx";
 
-const OrdersTableTopPanelSearchInput = ({...props}) => {
-    const dispatch = useDispatch();
-    const timeoutRef = useRef(null);
-    const [inputValue, setInputValue] = useState(useSelector(state => state.orders.searchInput));
+const OrdersTableTopPanelSearchInput = ({ ...props }) => {
+  const dispatch = useDispatch();
+  const timeoutRef = useRef(null);
+  const [inputValue, setInputValue] = useState(
+    useSelector((state) => state.orders.searchInput),
+  );
 
-    const handleSearchInputChange = (event) => {
-        const value = event.target.value;
-        // Natychmiast aktualizuj lokalny stan
-        setInputValue(value);
+  const handleSearchInputChange = (event) => {
+    const value = event.target.value;
+    // Natychmiast aktualizuj lokalny stan
+    setInputValue(value);
 
-        if (timeoutRef.current) {
-            clearTimeout(timeoutRef.current);
-        }
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
 
-        // Ustaw debouncing do zapisu do stanu Redux
-        timeoutRef.current = setTimeout(() => {
-            dispatch(setSearchInput(value));
-        }, 500);
-    };
-    
+    // Ustaw debouncing do zapisu do stanu Redux
+    timeoutRef.current = setTimeout(() => {
+      dispatch(setSearchInput(value));
+    }, 500);
+  };
 
-    return (
-        <input
-            value={inputValue}
-            type="text"
-            onChange={handleSearchInputChange}
-            placeholder="Search..."
-            {...props}
-        />
-    );
+  return (
+    <InputWithIcon
+      className={s.input}
+      value={inputValue}
+      type="text"
+      onChange={handleSearchInputChange}
+      placeholder="Search..."
+      {...props}
+    />
+  );
 };
 
 export default OrdersTableTopPanelSearchInput;
