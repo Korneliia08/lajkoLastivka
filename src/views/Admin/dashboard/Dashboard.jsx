feecda670138a3b5368cc6a27fe6db6cc0679ed3
AlfaGruisPL <alfagruis@gmail.com>
2025-01-29 16:03:02 +0100

    stage2

--- src/views/Admin/dashboard/Dashboard.jsx
+++ src/views/Admin/dashboard/Dashboard.jsx
@@ -2,54 +2,69 @@ import s from "./Dashboard.module.scss";
 import InfoTile from "../../../components/ui/statisitc/infoTile/InfoTile.jsx";
 import PanelTitle from "../../../components/layot/panelTitle/PanelTitle.jsx";
 import LikeUnlikeCharBlock from "../../../components/features/statistics/likeUnlikeCharBlock/LikeUnlikeCharBlock.jsx";
-import GeneralStatsCharBlock
-    from "../../../components/features/statistics/generalStatsCharBlock/GeneralStatsCharBlock.jsx";
+import GeneralStatsCharBlock from "../../../components/features/statistics/generalStatsCharBlock/GeneralStatsCharBlock.jsx";
 import OutletPanelScroll from "../../../components/ui/outletPanelScroll/OutletPanelScroll.jsx";
-import MarketplacesOrdersCharBlock
-    from "../../../components/features/statistics/marketplacesOrdersCharBlock/MarketplacesOrdersCharBlock.jsx";
+import MarketplacesOrdersCharBlock from "../../../components/features/statistics/marketplacesOrdersCharBlock/MarketplacesOrdersCharBlock.jsx";
 import useFetch from "@hooks/useFetch.js";
-import {blockStatsInitialData} from "./blockStatsInitialData.js";
-import {CgMail, CgRead} from "react-icons/cg";
-import {GiClick} from "react-icons/gi";
-
+import { blockStatsInitialData } from "./blockStatsInitialData.js";
+import { CgMail, CgRead } from "react-icons/cg";
+import { GiClick } from "react-icons/gi";
+import { MdOutlineRateReview } from "react-icons/md";
 
 const Dashboard = (props) => {
-    const {data, loading} = useFetch('dashboardPage/BlockData', {
-        default: blockStatsInitialData
-    });
+  const { data, loading } = useFetch("dashboardPage/BlockData", {
+    default: blockStatsInitialData,
+  });
 
-
-    return (
-        <>
-            <PanelTitle title={'Аналітика - цього місяця'}
-                        subTitle={'Актуальні дані з усіх Ваших маркетплейсів'}/>
-            <OutletPanelScroll>
-                <div className={s.dashboardContainer}>
-                    <div className={s.topRow}>
-
-                        <InfoTile icon={<CgMail/>} value={data.sendInMonth.current}
-                                  secondValue={data.sendInMonth.change} title={'Надіслані повідомлення'}/>
-                        <InfoTile icon={<CgRead/>} value={data.readInMonth.current}
-                                  secondValue={data.readInMonth.change} title={'Прочитані повідомлення'}
-                                  secondValueColor={'red'}/>
-                        <InfoTile icon={<GiClick/>} value={data.clickInMonth.current}
-                                  secondValue={data.clickInMonth.change} title={'Відвідані посилання'}
-                                  secondValueColor={'red'}/>
-                        <InfoTile title={'Написані відгуки'}/>
-                    </div>
-                    <div className={s.div3}>
-                        <GeneralStatsCharBlock/>
-                    </div>
-                    <div className={s.likeUnlike}>
-                        <LikeUnlikeCharBlock/>
-                    </div>
-                    <div className={s.orders}>
-                        <MarketplacesOrdersCharBlock/>
-                    </div>
-                    <div className={s.div4}></div>
-                </div>
-            </OutletPanelScroll>
-        </>
-    )
-}
+  return (
+    <>
+      <PanelTitle
+        title={"Аналітика - цього місяця"}
+        subTitle={"Актуальні дані з усіх Ваших маркетплейсів"}
+      />
+      <OutletPanelScroll>
+        <div className={s.dashboardContainer}>
+          <div className={s.topRow}>
+            <InfoTile
+              icon={<CgMail />}
+              value={data.sendInMonth.current}
+              secondValue={data.sendInMonth.change}
+              title={"Надіслані повідомлення"}
+            />
+            <InfoTile
+              icon={<CgRead />}
+              value={data.readInMonth.current}
+              secondValue={data.readInMonth.change}
+              title={"Прочитані повідомлення"}
+              secondValueColor={"red"}
+            />
+            <InfoTile
+              icon={<GiClick />}
+              value={data.clickInMonth.current}
+              secondValue={data.clickInMonth.change}
+              title={"Відвідані посилання"}
+              secondValueColor={"red"}
+            />
+            <InfoTile
+              value={data.writeOpinions.current}
+              secondValue={data.writeOpinions.change}
+              icon={<MdOutlineRateReview />}
+              title={"Написані відгуки"}
+            />
+          </div>
+          <div className={s.div3}>
+            <GeneralStatsCharBlock />
+          </div>
+          <div className={s.likeUnlike}>
+            <LikeUnlikeCharBlock />
+          </div>
+          <div className={s.orders}>
+            <MarketplacesOrdersCharBlock />
+          </div>
+          <div className={s.div4}></div>
+        </div>
+      </OutletPanelScroll>
+    </>
+  );
+};
 export default Dashboard;
