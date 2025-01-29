663a986b8b69f250cf5576c130a2755d998a44d1
Korneliia Mushak <jackiemaistrenko@gmail.com>
2025-01-29 16:14:23 +0100

    create a project

--- src/views/Admin/Marketplace/addAndEditMarketPlace/dataAccountToShops/dataAccountToShopsModal/DataAccountToShopsModal.jsx
+++ src/views/Admin/Marketplace/addAndEditMarketPlace/dataAccountToShops/dataAccountToShopsModal/DataAccountToShopsModal.jsx
@@ -1,78 +1,80 @@
 import s from "./DataAccountToShopsModal.module.scss";
-import { FaX } from "react-icons/fa6";
+import {FaX} from "react-icons/fa6";
 import RowOfFormAddAndEditShop from "@/components/ui/rowOfFormAddAndEditShop/RowOfFormAddAndEditShop.jsx";
-import { useDispatch, useSelector } from "react-redux";
-import { marketplaceSetField } from "@/views/Admin/Marketplace/addAndEditMarketPlace/marketplaceFormSlice.js";
+import {useDispatch, useSelector} from "react-redux";
+import {marketplaceSetField} from "@/views/Admin/Marketplace/addAndEditMarketPlace/marketplaceFormSlice.js";
 import api from "@/providers/interceptors/refreshToken.interceptor.js";
-import { toast } from "react-hot-toast";
-import { useState } from "react";
+import {toast} from "react-hot-toast";
+import {useState} from "react";
 
-const DataAccountToShopsModal = ({ controller, ...props }) => {
-  const dispatch = useDispatch();
-  const [isLoading, setLoading] = useState(false);
-  const {
-    rozetkaLogin,
-    rozetkaPassword,
-    isConnect: isConnect,
-  } = useSelector((state) => state.marketplaceForm);
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    dispatch(marketplaceSetField({ field: name, value }));
-  };
+const DataAccountToShopsModal = ({controller, ...props}) => {
+    const dispatch = useDispatch();
+    const [isLoading, setLoading] = useState(false);
+    const {
+        rozetkaLogin,
+        rozetkaPassword,
+        isConnect: isConnect,
+    } = useSelector((state) => state.marketplaceForm);
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        dispatch(marketplaceSetField({field: name, value}));
+    };
 
-  async function tryConnect() {
-    return api.post("/stores/checkConnection", {
-      login: rozetkaLogin,
-      password: btoa(unescape(encodeURIComponent(rozetkaPassword))),
-    });
-  }
-
-  async function connect() {
-    setLoading(true);
-
-    const isConnect = await toast.promise(tryConnect(), {
-      //todo translate
-      loading: "Trwa próba połączenia z sklepem",
-      success: <b>Połączenie z sklepem udane</b>,
-      error: <b>Błąd podłączenia do sklepu.</b>,
-    });
-    setLoading(false);
-    dispatch(
-      marketplaceSetField({
-        field: "isConnect",
-        value: isConnect,
-      }),
-    );
-    if (isConnect) {
-      controller.closeModal();
+    async function tryConnect() {
+        return api.post("/stores/checkConnection", {
+            login: rozetkaLogin,
+            password: btoa(unescape(encodeURIComponent(rozetkaPassword))),
+        });
     }
-  }
 
-  // controller.closeModal();
+    async function connect() {
+        setLoading(true);
 
-  return (
-    <div className={s.dataAccountToShopsModalContainer}>
-      <h2>TITLE </h2>
-      {isConnect ? "CONNECT" : "No connect"}
-      <FaX onClick={controller.closeModal}></FaX>
-      <RowOfFormAddAndEditShop
-        onChange={handleChange}
-        name={"rozetkaLogin"}
-        value={rozetkaLogin}
-        title={"Логін"}
-        describe={"Введіть логін до кабінету магазину"}
-      />
-      <RowOfFormAddAndEditShop
-        onChange={handleChange}
-        name={"rozetkaPassword"}
-        value={rozetkaPassword}
-        title={"Пароль"}
-        type={"password"}
-        describe={"Введіть пароль" + " до кабінету" + " магазину"}
-      />
-      <button onClick={controller.closeModal}>Anuluj</button>
-      <button onClick={connect}>Podłącz</button>
-    </div>
-  );
+        const isConnect = await toast.promise(tryConnect(), {
+            loading: "Відбувається спроба з'єднання з магазином",
+            success: <b>Підключення із магазином успішно проведено</b>,
+            error: <b>Помилка підключення із магазином</b>,
+        });
+        setLoading(false);
+        dispatch(
+            marketplaceSetField({
+                field: "isConnect",
+                value: isConnect,
+            }),
+        );
+        if (isConnect) {
+            controller.closeModal();
+        }
+    }
+
+    // controller.closeModal();
+
+    return (
+        <div className={s.dataAccountToShopsModalContainer}>
+            <h2 className={s.title}>Підключення магазину до адміністративної панелі</h2>
+            <FaX onClick={controller.closeModal} className={s.close}></FaX>
+            <div className={s.containerForRows}>
+                <RowOfFormAddAndEditShop
+                    onChange={handleChange}
+                    name={"rozetkaLogin"}
+                    value={rozetkaLogin}
+                    title={"Логін"}
+                    describe={"Введіть логін до кабінету магазину"}
+                />
+                <RowOfFormAddAndEditShop
+                    onChange={handleChange}
+                    name={"rozetkaPassword"}
+                    value={rozetkaPassword}
+                    title={"Пароль"}
+                    type={"password"}
+                    describe={"Введіть пароль" + " до кабінету" + " магазину"}
+                />
+            </div>
+            <div className={s.blockForBtns}>
+                <button onClick={controller.closeModal}>Скасувати</button>
+                <button onClick={connect}>Підключити</button>
+            </div>
+        </div>
+    );
 };
 export default DataAccountToShopsModal;
