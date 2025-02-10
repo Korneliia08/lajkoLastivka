6e1d37308659ba789bc40f5bed6d9261c66f81c1
Korneliia Mushak <jackiemaistrenko@gmail.com>
2025-02-10 17:56:14 +0100

    create a project

--- src/views/Admin/orders/ordersTable/ordersTableTopPanel/manualMessageDispatch/manualDispatchContent/ManualDispatchContent.jsx
+++ src/views/Admin/orders/ordersTable/ordersTableTopPanel/manualMessageDispatch/manualDispatchContent/ManualDispatchContent.jsx
@@ -1,103 +1,104 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import s from "./ManualDispatchContent.module.scss";
 import LoadImageFromComputer from "@/components/ui/loadImageFromComputer/LoadImageFromComputer.jsx";
-import { useSelector } from "react-redux";
+import {useSelector} from "react-redux";
 
-const ManualDispatchContent = ({ data, setData, loadDefault, ...props }) => {
-  const selectedOrders = useSelector((state) => state.orders.selectedOrders);
-  const [keepInMemory, setKeepInMemory] = useState(false);
+const ManualDispatchContent = ({data, setData, loadDefault, ...props}) => {
+    const selectedOrders = useSelector((state) => state.orders.selectedOrders);
+    const [keepInMemory, setKeepInMemory] = useState(false);
 
-  useEffect(() => {
-    // Sprawdzanie czy dane są zapisane w localStorage
-    const savedMessage = localStorage.getItem("message");
-    const imageData = localStorage.getItem("bannerImg");
-    const imageLogoData = localStorage.getItem("logoImg");
-    const savedKeepInMemory = JSON.parse(localStorage.getItem("keepInMemory"));
+    useEffect(() => {
+        // Sprawdzanie czy dane są zapisane w localStorage
+        const savedMessage = localStorage.getItem("message");
+        const imageData = localStorage.getItem("bannerImg");
+        const imageLogoData = localStorage.getItem("logoImg");
+        const savedKeepInMemory = JSON.parse(localStorage.getItem("keepInMemory"));
 
-    if (savedMessage && imageData && imageLogoData) {
-      setData({
-        ...data,
-        message: savedMessage,
-        imageLogoData: imageLogoData,
-        imageData: imageData,
-      });
-    }
+        if (savedMessage && imageData && imageLogoData) {
+            setData({
+                ...data,
+                message: savedMessage,
+                imageLogoData: imageLogoData,
+                imageData: imageData,
+            });
+        }
 
-    if (savedKeepInMemory) {
-      setKeepInMemory(savedKeepInMemory);
-    }
-  }, []);
+        if (savedKeepInMemory) {
+            setKeepInMemory(savedKeepInMemory);
+        }
+    }, []);
 
-  useEffect(() => {
-    if (keepInMemory) {
-      // Zapisz dane do localStorage
-      localStorage.setItem("message", data.message);
-      localStorage.setItem("logoImg", data.imageLogoData);
-      localStorage.setItem("bannerImg", data.imageData);
-      localStorage.setItem("keepInMemory", JSON.stringify(keepInMemory));
-      console.log(keepInMemory);
-    } else {
-      // Jeśli checkbox nie jest zaznaczony, usuwamy dane
-      localStorage.removeItem("message");
-      localStorage.removeItem("logoImg");
-      localStorage.removeItem("bannerImg");
-      localStorage.removeItem("keepInMemory");
-    }
-  }, [keepInMemory, data.message, data.imageData, data.imageLogoData]);
+    useEffect(() => {
+        if (keepInMemory) {
+            // Zapisz dane do localStorage
+            localStorage.setItem("message", data.message);
+            localStorage.setItem("logoImg", data.imageLogoData);
+            localStorage.setItem("bannerImg", data.imageData);
+            localStorage.setItem("keepInMemory", JSON.stringify(keepInMemory));
+            console.log(keepInMemory);
+        } else {
+            // Jeśli checkbox nie jest zaznaczony, usuwamy dane
+            localStorage.removeItem("message");
+            localStorage.removeItem("logoImg");
+            localStorage.removeItem("bannerImg");
+            localStorage.removeItem("keepInMemory");
+        }
+    }, [keepInMemory, data.message, data.imageData, data.imageLogoData]);
 
-  const handleCheckboxChange = () => {
-    setKeepInMemory(!keepInMemory);
-  };
+    const handleCheckboxChange = () => {
+        setKeepInMemory(!keepInMemory);
+    };
 
-  return (
-    <div className={s.manualDispatchContentContainer}>
-      <div className={s.blockForTitle}>
-        <h2 className={s.title}>Розсилка повідомлень</h2>
-        <div className={s.information}>
-          <span className={s.titleOfItem}>Вибрані замовлення:</span>
-          <span className={s.value}>{selectedOrders.length}</span>
-        </div>
-      </div>
+    return (
+        <div className={s.manualDispatchContentContainer}>
+            <div className={s.blockForTitle}>
+                <h2 className={s.title}>Розсилка
+                    повідомлень <span className={s.titleOfItem}>(вибрані замовлення:</span>
+                    <span className={s.value}>{selectedOrders.length})</span></h2>
+                <div className={s.information}>
+                    <span className={s.describe}>Під час першого відображення завантажується стандартний шаблон, створений при підключенні магазину. Ви можете його редагувати, зберігати або відновлювати стандартні налаштування.</span>
+                    <button className={s.btnKeep} onClick={loadDefault}>
+                        Відновити типові налаштування
+                    </button>
+                </div>
+            </div>
 
-      <div className={s.containerForLoad}>
-        <LoadImageFromComputer
-          onChange={(event) => setData({ ...data, imageLogoData: event })}
-          title={"Лого"}
-          value={data.imageLogoData}
-          describe={"Лого буде відображено на хедері"}
-        />
-        <LoadImageFromComputer
-          onChange={(event) => setData({ ...data, imageData: event })}
-          title={"Тло для хедера"}
-          value={data.imageData}
-          describe={"Тло використовуватиметься у хедері"}
-        />
-      </div>
+            <div className={s.containerForLoad}>
+                <LoadImageFromComputer
+                    onChange={(event) => setData({...data, imageLogoData: event})}
+                    title={"Лого"}
+                    value={data.imageLogoData}
+                    describe={"Лого буде відображено на хедері"}
+                />
+                <LoadImageFromComputer
+                    onChange={(event) => setData({...data, imageData: event})}
+                    title={"Тло для хедера"}
+                    value={data.imageData}
+                    describe={"Тло використовуватиметься у хедері"}
+                />
+            </div>
 
-      <div className={s.containerForTextarea}>
-        <span className={s.title}>Шаблон повідомлення</span>
-        <textarea
-          className={s.textareaStyle}
-          value={data.message}
-          onChange={(event) =>
-            setData({ ...data, message: event.target.value })
-          }
-        ></textarea>
-        <span>
-          Keep in memory:&nbsp;&nbsp;&nbsp;
-          <input
-            type="checkbox"
-            checked={keepInMemory}
-            onChange={handleCheckboxChange}
-          />
-          <button className={s.btnKeep} onClick={loadDefault}>
-            Load default
-          </button>
+            <div className={s.containerForTextarea}>
+                <span className={s.title}>Шаблон повідомлення</span>
+                <textarea
+                    className={s.textareaStyle}
+                    value={data.message}
+                    onChange={(event) =>
+                        setData({...data, message: event.target.value})
+                    }
+                ></textarea>
+                <span className={s.memorySpan}>
+          Зберегти у пам’яті:&nbsp;&nbsp;&nbsp;
+                    <input
+                        type="checkbox"
+                        checked={keepInMemory}
+                        onChange={handleCheckboxChange}
+                    />
         </span>
-        <br />
-      </div>
-    </div>
-  );
+                <br/>
+            </div>
+        </div>
+    );
 };
 
 export default ManualDispatchContent;
