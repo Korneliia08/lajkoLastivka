fe60771697bcde2b067a231d1623a4130161c993
Korneliia Mushak <jackiemaistrenko@gmail.com>
2025-02-12 21:48:40 +0100

    create a project

--- src/views/Admin/opinions/cardOfUser/CardOfUser.jsx
+++ src/views/Admin/opinions/cardOfUser/CardOfUser.jsx
@@ -2,14 +2,18 @@ import s from "./CardOfUser.module.scss";
 import NumberOfOrderAndEtaps from "@/views/Admin/opinions/cardOfUser/numberOfOrderAndEtaps/NumberOfOrderAndEtaps.jsx";
 import DataOfUser from "@/views/Admin/opinions/dataOfUser/DataOfUser.jsx";
 import BlockOfIcons from "@/views/Admin/opinions/cardOfUser/blockOfIcons/BlockOfIcons.jsx";
+import UnfoldedCard from "@/views/Admin/opinions/cardOfUser/unfoldedCard/UnfoldedCard.jsx";
 
 
 const CardOfUser = ({...props}) => {
     return (
-        <div className={s.cardOfUserContainer}>
-            <NumberOfOrderAndEtaps/>
-            <DataOfUser/>
-            <BlockOfIcons/>
+        <div className={s.container}>
+            <div className={s.cardOfUserContainer}>
+                <NumberOfOrderAndEtaps/>
+                <DataOfUser/>
+                <BlockOfIcons/>
+            </div>
+            <UnfoldedCard/>
         </div>
     )
 }
---
+++ src/views/Admin/opinions/cardOfUser/unfoldedCard/UnfoldedCard.jsx
@@ -0,0 +1,11 @@
+import s from "./UnfoldedCard.module.scss";
+
+
+const UnfoldedCard = ({...props}) => {
+return (
+<div className={s.unfoldedCardContainer}>
+    UnfoldedCard
+</div>
+)
+}
+export default UnfoldedCard;
