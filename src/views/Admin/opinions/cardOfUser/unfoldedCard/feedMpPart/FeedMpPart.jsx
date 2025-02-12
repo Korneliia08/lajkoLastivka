d822e8f47d5d03388770696a8582b5be5af1bdf2
Korneliia Mushak <jackiemaistrenko@gmail.com>
2025-02-12 22:27:24 +0100

    create a project

--- src/views/Admin/opinions/cardOfUser/unfoldedCard/feedMpPart/FeedMpPart.jsx
+++ src/views/Admin/opinions/cardOfUser/unfoldedCard/feedMpPart/FeedMpPart.jsx
@@ -6,8 +6,10 @@ const FeedMpPart = ({...props}) => {
     return (
         <div className={s.feedMpPartContainer}>
             <h4 className={s.title}>FeedMp</h4>
-            <p className={s.comment}><ImQuotesRight className={s.quotesLeft}/>Все чудово!<ImQuotesRight
-                className={s.quotesRight}/></p>
+            <p className={s.comment}><ImQuotesRight className={s.quotesLeft}/>Годинник виглядає стильно, але батарея
+                швидко сідає, а повідомлення іноді приходять із затримкою.!<ImQuotesRight
+                    className={s.quotesRight}/></p>
+            {/*<p className={s.comment}>Покупець не залишив відгук</p> to gdy 1-3 revievs, ale na feedMP nie zostawiono opinii*/}
         </div>
     )
 }
--- src/views/Admin/opinions/cardOfUser/unfoldedCard/rozetkaPart/RozetkaPart.jsx
+++ src/views/Admin/opinions/cardOfUser/unfoldedCard/rozetkaPart/RozetkaPart.jsx
@@ -1,11 +1,16 @@
 import s from "./RozetkaPart.module.scss";
+import {ImQuotesRight} from "react-icons/im";
 
 
 const RozetkaPart = ({...props}) => {
-return (
-<div className={s.rozetkaPartContainer}>
-    RozetkaPart
-</div>
-)
+    return (
+        <div className={s.rozetkaPartContainer}>
+            <h4 className={s.title}>Розетка</h4>
+            <p className={s.comment}><ImQuotesRight className={s.quotesLeft}/>Смарт-годинник чудовий! Стильний дизайн,
+                довго тримає заряд і працює без збоїв. Рекомендую!<ImQuotesRight
+                    className={s.quotesRight}/></p>
+            {/*<p className={s.comment}>Покупець не залишив відгук</p> to gdy 4-5 revievs, ale na rozetce nie zostawiono opinii*/}
+        </div>
+    )
 }
 export default RozetkaPart;
