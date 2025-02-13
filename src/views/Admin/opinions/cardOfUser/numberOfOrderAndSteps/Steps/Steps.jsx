a91e50af09adad00ba1ffed0f7fe19cca40cd43b
Korneliia Mushak <jackiemaistrenko@gmail.com>
2025-02-13 14:59:51 +0100

    create a project

--- src/views/Admin/opinions/cardOfUser/numberOfOrderAndSteps/Steps/Steps.jsx
+++ src/views/Admin/opinions/cardOfUser/numberOfOrderAndSteps/Steps/Steps.jsx
@@ -1,79 +1,75 @@
 import s from "./Steps.module.scss";
 
-import { IoSendOutline } from "react-icons/io5";
-import {
-  TbHandClick,
-  TbMessage2Check,
-  TbShoppingBagHeart,
-} from "react-icons/tb";
+import {IoSendOutline} from "react-icons/io5";
+import {TbHandClick, TbMessage2Check, TbShoppingBagHeart,} from "react-icons/tb";
 import Step from "@/views/Admin/opinions/cardOfUser/numberOfOrderAndSteps/Steps/Step/Step.jsx";
-import { MdOutlineStarBorder } from "react-icons/md";
+import {MdOutlineStarBorder} from "react-icons/md";
 
-const Steps = ({ data, ...props }) => {
-  const localOpinion = data.order.items[0].localOpinion;
-  const stars = localOpinion.ratingScore;
-  const opinion = data.order.items[0].opinion;
-  console.log(opinion);
+const Steps = ({data, ...props}) => {
+    const localOpinion = data.order.items[0].localOpinion;
+    const stars = localOpinion.ratingScore;
+    const opinion = data.order.items[0].opinion;
+    console.log(opinion);
 
-  return (
-    <div className={s.etapsContainer}>
-      <Step
-        icon={
-          <IoSendOutline
-            size={14}
-            style={{ color: "black" }}
-            title={"Повідомлення надіслано"}
-          />
-        }
-      />
-      <Step
-        icon={
-          <TbMessage2Check
-            size={14}
-            style={{ color: "black" }}
-            title={"Повідомлення прочитано"}
-          />
-        }
-      />
-      <Step
-        icon={
-          <TbHandClick
-            size={14}
-            style={{ color: "black" }}
-            title={"Посилання відвідано"}
-          />
-        }
-      />
-      <Step
-        icon={
-          <MdOutlineStarBorder
-            size={14}
-            style={{
-              color: localOpinion.ratingScore <= 3 ? "#f77" : "#49ff54",
-            }}
-            title={"Замовлення оцінено"}
-          />
-        }
-      />
+    return (
+        <div className={s.etapsContainer}>
+            <Step
+                icon={
+                    <IoSendOutline
+                        size={14}
+                        style={{color: "#011c5d"}}
+                        title={"Повідомлення надіслано"}
+                    />
+                }
+            />
+            <Step
+                icon={
+                    <TbMessage2Check
+                        size={14}
+                        style={{color: "#011c5d"}}
+                        title={"Повідомлення прочитано"}
+                    />
+                }
+            />
+            <Step
+                icon={
+                    <TbHandClick
+                        size={14}
+                        style={{color: "#011c5d"}}
+                        title={"Посилання відвідано"}
+                    />
+                }
+            />
+            <Step
+                icon={
+                    <MdOutlineStarBorder
+                        size={14}
+                        style={{
+                            color: localOpinion.ratingScore <= 3 ? "rgb(255, 0, 0)" : "rgb(21, 128, 77)",
+                        }}
+                        title={"Замовлення оцінено"}
+                    />
+                }
+            />
 
-      <Step
-        disable={!(opinion || localOpinion.text)}
-        icon={
-          <TbShoppingBagHeart
-            size={14}
-            style={{ color: "black" }}
-            title={"Відгук написано"}
-          />
-        }
-      />
+            <Step
+                disable={!(opinion || localOpinion.text)}
+                icon={
+                    <TbShoppingBagHeart
+                        size={14}
+                        style={{color: "#011c5d"}}
+                        title={"Відгук написано"}
+                    />
+                }
+            />
 
-      {/*tu nie poswiecamy kolko*/}
-      {/*    4-5 - opinia nie napisana Відгук не залишено*/}
-      {/*1-3 - ( gdy opinia na feedmp nie napisana,i na rozetce tez nie) - opinia nie napisana - Відгук не залишено*/}
+            {/*tu nie poswiecamy kolko*/}
+            {/*    4-5 - opinia nie napisana Відгук не залишено*/}
+            {/*1-3 - ( gdy opinia na feedmp nie napisana,i na rozetce tez nie) - opinia nie napisana - Відгук не залишено*/}
 
-      {/*tu poswiecamy ze aktywne kolko jednak*/}
-      {/*1-3 - (gdy opinia na feedmp nie napisana,ale na rozetce ta) - opinia napisana,ale na Rozetce Відгук залишено,але на розетці */}
-    </div>
-  );
+            {/*tu poswiecamy ze aktywne kolko jednak*/}
+            {/*1-3 - (gdy opinia na feedmp nie napisana,ale na rozetce ta) - opinia napisana,ale na Rozetce Відгук залишено,але на розетці */}
+        </div>
+    );
 };
 export default Steps;
