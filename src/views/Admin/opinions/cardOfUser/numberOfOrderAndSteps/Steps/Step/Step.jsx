081fe02d150bb82a1087c108832cda71d2f988a1
AlfaGruisPL <alfagruis@gmail.com>
2025-02-13 14:54:47 +0100

    stage2

--- src/views/Admin/opinions/cardOfUser/numberOfOrderAndSteps/Steps/Step/Step.jsx
+++ src/views/Admin/opinions/cardOfUser/numberOfOrderAndSteps/Steps/Step/Step.jsx
@@ -1,8 +1,9 @@
 import s from "./Step.module.scss";
+import cn from "@/functions/cn.js";
 
 const Step = ({ disable, icon, ...props }) => {
   return (
-    <div className={s.mainContainer}>
+    <div className={cn(s.mainContainer, disable && s.disabled)}>
       <div className={s.etapContainer}>{icon}</div>
       <div className={s.afterDiv}></div>
     </div>
