3dc697a205999a01ce383f785b6840cc76bd95ec
Korneliia Mushak <jackiemaistrenko@gmail.com>
2025-02-20 14:52:03 +0100

    create a project

--- src/views/Admin/filterPageSettings/FilterPageSettings.jsx
+++ src/views/Admin/filterPageSettings/FilterPageSettings.jsx
@@ -3,11 +3,12 @@ import {useParams} from "react-router-dom";
 import PanelTitle from "@/components/layot/panelTitle/PanelTitle.jsx";
 import OutletPanelScroll from "@/components/ui/outletPanelScroll/OutletPanelScroll.jsx";
 import FilterPageSettingsSelectParketplace
-  from "@/views/Admin/filterPageSettings/filterPageSettingsSelectParketplace/FilterPageSettingsSelectParketplace.jsx";
+    from "@/views/Admin/filterPageSettings/filterPageSettingsSelectParketplace/FilterPageSettingsSelectParketplace.jsx";
 import {useQuill} from "react-quilljs";
 import {useEffect} from "react";
 import useFetch from "@hooks/useFetch.js";
 import api from "@/providers/interceptors/refreshToken.interceptor.js";
+import {FaRegSave} from "react-icons/fa";
 
 const FilterPageSettings = ({...props}) => {
     const {id} = useParams();
@@ -23,44 +24,44 @@ const FilterPageSettings = ({...props}) => {
     } = useQuill();
     const {data} = useFetch(`/stores/filterPageConfig/${id}`);
 
-  useEffect(() => {
-    if (
-      id != undefined &&
-      firstPageQuill &&
-      commentWriteTextAreaTitlePageQuill &&
-      successPageQuill &&
-      commentSendPageQuill &&
-      commentWritePageQuill &&
-      commentWriteTextAreaTitlePageQuill &&
-      data.filterPageContent
-    ) {
-      firstPageQuill.clipboard.dangerouslyPasteHTML(
-        data.filterPageContent.welcomePage,
-      );
+    useEffect(() => {
+        if (
+            id != undefined &&
+            firstPageQuill &&
+            commentWriteTextAreaTitlePageQuill &&
+            successPageQuill &&
+            commentSendPageQuill &&
+            commentWritePageQuill &&
+            commentWriteTextAreaTitlePageQuill &&
+            data.filterPageContent
+        ) {
+            firstPageQuill.clipboard.dangerouslyPasteHTML(
+                data.filterPageContent.welcomePage,
+            );
 
-      successPageQuill.clipboard.dangerouslyPasteHTML(
-        data.filterPageContent.successPage,
-      );
+            successPageQuill.clipboard.dangerouslyPasteHTML(
+                data.filterPageContent.successPage,
+            );
 
-      commentWritePageQuill.clipboard.dangerouslyPasteHTML(
-        data.filterPageContent.writeCommentPage,
-      );
+            commentWritePageQuill.clipboard.dangerouslyPasteHTML(
+                data.filterPageContent.writeCommentPage,
+            );
 
-      commentWriteTextAreaTitlePageQuill.clipboard.dangerouslyPasteHTML(
-        data.filterPageContent.writeCommentLabelPage,
-      );
-      commentSendPageQuill.clipboard.dangerouslyPasteHTML(
-        data.filterPageContent.commentSendPageQuill,
-      );
-    }
-  }, [
-    firstPageQuill,
-    successPageQuill,
-    commentSendPageQuill,
-    commentWritePageQuill,
-    commentWriteTextAreaTitlePageQuill,
-    data,
-  ]);
+            commentWriteTextAreaTitlePageQuill.clipboard.dangerouslyPasteHTML(
+                data.filterPageContent.writeCommentLabelPage,
+            );
+            commentSendPageQuill.clipboard.dangerouslyPasteHTML(
+                data.filterPageContent.commentSendPageQuill,
+            );
+        }
+    }, [
+        firstPageQuill,
+        successPageQuill,
+        commentSendPageQuill,
+        commentWritePageQuill,
+        commentWriteTextAreaTitlePageQuill,
+        data,
+    ]);
 
     async function sendData() {
         const dataToSend = {
@@ -82,30 +83,35 @@ const FilterPageSettings = ({...props}) => {
                 inner={true}
                 title={"Управління контентом:"}
                 subTitle={
-                    "У цій вкладці ви можете легко змінювати або додавати контент, який відображатиметься на сторінках, де клієнти залишають відгуки про товар."
+                    "У цій вкладці можна легко змінювати або додавати контент, який відображатиметься на сторінках, де клієнти залишають відгуки про товар."
                 }
+                buttonText={"Зберегти"}
+                onClick={sendData}
+                buttonIcon={<FaRegSave/>}
             />
             <div className={s.filterPageSettingsContainer}>
                 <div className={s.selectMarketPlace}>
                     <FilterPageSettingsSelectParketplace/>
-                    <button onClick={sendData}>Send</button>
                 </div>
-                <div className={s.block}>
-                    <div className={s.titleContainer}>
-                        <span className={s.title}>Strona główna</span>
-                        <span className={s.describe}>Tam gdzie gwiazdki</span>
+                <div className={s.containerForBlocks}>
+                    <div className={s.block}>
+                        <div className={s.titleContainer}>
+                            <span className={s.title}>Сторінка фільтрації</span>
+                            <span className={s.describe}>Там де клієнт обирає кількість зірочок</span>
+                        </div>
+                        <div className={s.quillContainer} style={{width: 500, height: 300}}>
+                            <div ref={firstPageRef}/>
+                        </div>
                     </div>
-                    <div className={s.quillContainer} style={{width: 500, height: 300}}>
-                        <div ref={firstPageRef}/>
-                    </div>
-                </div>
-                <div className={s.block}>
-                    <div className={s.titleContainer}>
-                        <span className={s.title}>Sukces ppage</span>
-                        <span className={s.describe}>Tam gdzie instrukcja</span>
-                    </div>
-                    <div className={s.quillContainer} style={{width: 500, height: 300}}>
-                        <div ref={successPageRef}/>
+                    <div className={s.block}>
+                        <div className={s.titleContainer}>
+                            <span className={s.title}>Сторінка подяки та інструкції</span>
+                            <span className={s.describe}>Ця сторінка відображається клієнтам, які оцінили замовлення на 4 або 5 зірок.
+                                Вона містить подяку за оцінку та інструкцію, як залишити детальний відгук на ROZETKA.</span>
+                        </div>
+                        <div className={s.quillContainer} style={{width: 500, height: 300}}>
+                            <div ref={successPageRef}/>
+                        </div>
                     </div>
                 </div>
 
