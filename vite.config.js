0d331f99306c9f4aa6be689601a0c659b5a3d957
AlfaGruisPL <alfagruis@gmail.com>
2025-02-20 16:06:01 +0100

    Auto commit version 0.0.36

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,28 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import * as path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-            '@hooks': path.resolve(__dirname, './src/hooks'),
-        },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
     },
-    css: {
-        preprocessorOptions: {
-            scss: {
-                additionalData: `@use "@/assets/styles/variables.scss" as *;\n`,
-            },
-        },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/assets/styles/variables.scss" as *;\n`,
+      },
     },
-})
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "window",
+      },
+    },
+  },
+});
